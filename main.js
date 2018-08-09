4b5ca080ce0c04d885111451427935074735ece4
lstravers <stravers222@gmail.com>
2018-08-08 22:09:06 -0400

    modified files.

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 // MY VERSION
 
+//Select the DOM node for the element that will be clicked (the image).
+// Choose the event that will act as the trigger for the function (the mouse click).
+// Specify the function that you wish to run when the event occurs.
+
+<script>
+
+var button = document.querySelector('.button');
+button.addEventListener("click", function(event){
+
+})
+
+</script>
+
 // DEMO VERSION
 
 const calculator = document.querySelector(‘.calculator’)
